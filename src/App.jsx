93179aeaa5f8a4ed40423e1ed1ff44e91b78d327
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-02-10 19:56:57 +0530

    m

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Dashboard } from './components/dashboard'
+import { Landing } from './components/landing'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
+    <div>
+      <div style={{background: "black"}}>
+        Hi this is topBar
+      </div>
     <BrowserRouter>
-     
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/" element={<Landing></Landing>}></Route>
+      </Routes>
     </BrowserRouter>
+    </div>
   )
 }
 
