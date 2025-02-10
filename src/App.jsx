1244772b30599711fd2a138e3010050b6c378bbe
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-02-10 19:45:14 +0530

    rect routes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+     
+    </BrowserRouter>
+  )
+}
+
+export default App
