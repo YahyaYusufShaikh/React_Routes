cb925a8cae03fff8d266588d17b2642123ef27ff
YahyaYusufShaikh <yahyayusuf@rediffmail.com>
2025-02-12 20:35:14 +0530

    default

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
-import { Dashboard } from './components/dashboard'
-import { Landing } from './components/landing'
+import  Dashboard  from './components/dashboard'
+import  Landing  from './components/landing'
 
 function App() {
   return (
